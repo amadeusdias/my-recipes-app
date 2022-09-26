fced92a2585f5b0854bf56d74327c19dcd99e048
rafaelftourinho <rafaelftourinho@gmail.com>
2022-09-26 14:40:04 -0300

    :bug: resolvendo problemas na tela de login

--- src/App.js
+++ src/App.js
@@ -7,32 +7,22 @@ import AppRecipesProvider from './context/AppRecipesProvider';
 
 function App() {
   return (
-    <div className="meals">
-      {/* <span className="logo">TRYBE 2.0</span>
-       <object
-        className="rocksGlass"
-        type="image/svg+xml"
-        data={ rockGlass }
-      >
-        Glass
-      </object> */}
-      <AppRecipesProvider>
-        <BrowserRouter>
-          <Switch>
-            <Route exact path="/" component={ Login } />
-            <Route path="/meals" component={ Meals } />
-            <Route path="/drinks" />
-            <Route path="/meals/:id" />
-            <Route path="/drinks/:id" />
-            <Route path="/drinks/:id/in-progress" />
-            <Route path="/meals/:id/in-progress" />
-            <Route path="/profile" />
-            <Route path="/done-recipes" />
-            <Route path="/favorite-recipes" />
-          </Switch>
-        </BrowserRouter>
-      </AppRecipesProvider>
-    </div>
+    <AppRecipesProvider>
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/" component={ Login } />
+          <Route path="/meals" component={ Meals } />
+          <Route path="/drinks" />
+          <Route path="/meals/:id" />
+          <Route path="/drinks/:id" />
+          <Route path="/drinks/:id/in-progress" />
+          <Route path="/meals/:id/in-progress" />
+          <Route path="/profile" />
+          <Route path="/done-recipes" />
+          <Route path="/favorite-recipes" />
+        </Switch>
+      </BrowserRouter>
+    </AppRecipesProvider>
   );
 }
 
