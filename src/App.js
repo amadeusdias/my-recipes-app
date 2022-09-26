7f37e5ab0b29a00337fe63f5da401f0c13d22042
FabioMateus1510 <fabiomateus1510@gmail.com>
2022-09-25 21:34:06 -0300

    CSS da pagina de login

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
 import React from 'react';
-import './App.css';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import rockGlass from './images/rockGlass.svg';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Login from './pages/Login';
 import AppRecipesProvider from './context/AppRecipesProvider';
@@ -9,14 +7,14 @@ import AppRecipesProvider from './context/AppRecipesProvider';
 function App() {
   return (
     <div className="meals">
-      <span className="logo">TRYBE 2.0</span>
-      <object
+      {/* <span className="logo">TRYBE 2.0</span>
+       <object
         className="rocksGlass"
         type="image/svg+xml"
         data={ rockGlass }
       >
         Glass
-      </object>
+      </object> */}
       <AppRecipesProvider>
         <BrowserRouter>
           <Switch>
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,6 +1,8 @@
 import React, { useContext, useState } from 'react';
-import Header from '../components/Header';
 import AppRecipesContext from '../context/AppRecipesContext';
+import '../css/login.css';
+import logo from '../images/logoRecipesApp.png';
+import tomate from '../images/tomate.png';
 
 function Login() {
   const { setUserEmail } = useContext(AppRecipesContext);
@@ -41,36 +43,49 @@ function Login() {
   }
 
   return (
-    <div>
-      <Header />
-      <input
-        type="text"
-        data-testid="email-input"
-        // value={ userEmail }
-        name="email"
-        onChange={ (e) => {
-          setEmail(e.target.value);
-          validateButton();
-        } }
-      />
-      <input
-        type="text"
-        data-testid="password-input"
-        name="password"
-        // value={ userInfo.password }
-        onChange={ (p) => {
-          setUserPassword(p.target.value);
-          validateButton();
-        } }
-      />
-      <button
-        type="button"
-        data-testid="login-submit-btn"
-        onClick={ handleClick }
-        disabled={ disabled }
-      >
-        Enter
-      </button>
+    <div className="container-login-page">
+      <div className="container-top">
+        <img src={ logo } alt="logoRecipesApp" />
+      </div>
+      <div className="tomate">
+        <img src={ tomate } alt="tomate" />
+      </div>
+      <div className="container-down">
+        <h1 className="Title-login">LOGIN</h1>
+        <input
+          className="input-login"
+          type="email"
+          data-testid="email-input"
+          // value={ userEmail }
+          name="email"
+          placeholder="E-mail"
+          onChange={ (e) => {
+            setEmail(e.target.value);
+            validateButton();
+          } }
+        />
+        <input
+          className="input-login"
+          type="password"
+          data-testid="password-input"
+          // value={ userInfo.password }
+          name="password"
+          placeholder="Password"
+          onChange={ (p) => {
+            setUserPassword(p.target.value);
+            validateButton();
+          } }
+        />
+        <button
+          className="btn-enter"
+          type="button"
+          data-testid="login-submit-btn"
+          onClick={ handleClick }
+          disabled={ disabled }
+        >
+          ENTER
+        </button>
+      </div>
     </div>
   );
 }
