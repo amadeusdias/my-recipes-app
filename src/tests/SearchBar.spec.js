5ea7ce49b960728a27e23bde285cf6e49a02f95d
FabioMateus1510 <99938944+FabioMateus1510@users.noreply.github.com>
2022-09-27 15:47:40 -0300

    Update SearchBar.spec.js

--- src/tests/SearchBar.spec.js
+++ src/tests/SearchBar.spec.js
@@ -3,15 +3,15 @@ import userEvent from '@testing-library/user-event';
 import App from '../App';
 import renderWithRouter from './helpers/renderWithRouter';
 
-function accessToMeals() {
-  const email = screen.getByPlaceholderText(/e-mail/i);
-  const password = screen.getByPlaceholderText(/password/i);
-  const button = screen.getByTestId('login-submit-btn');
+// function accessToMeals() {
+// const email = screen.getByPlaceholderText(/e-mail/i);
+// const password = screen.getByPlaceholderText(/password/i);
+// const button = screen.getByTestId('login-submit-btn');
 
-  userEvent.type(email, 'test@test.com');
-  userEvent.type(password, '123456789');
-  userEvent.click(button);
-}
+// userEvent.type(email, 'test@test.com');
+// userEvent.type(password, '123456789');
+// userEvent.click(button);
+// }
 
 describe('test component <SearchBar />', () => {
   it('Testing inputs', () => {
