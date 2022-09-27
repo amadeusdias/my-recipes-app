a2d0c1bb55a237af41801f18f9ec359a341b1e0f
rafaelftourinho <rafaelftourinho@gmail.com>
2022-09-27 16:58:35 -0300

    :sparkles: Requisite 21

--- src/context/AppRecipesProvider.js
+++ src/context/AppRecipesProvider.js
@@ -4,11 +4,16 @@ import { useHistory } from 'react-router-dom';
 import AppRecipesContext from './AppRecipesContext';
 import { fetchMealsCards, fetchDrinksCards } from '../service/fetchCards';
 
+// const ZERO = 0;
+// const TWELVE = 12;
+
 function AppRecipesProvider({ children }) {
   const location = useHistory();
   const [userEmail, setUserEmail] = useState({
     email: '',
   });
+  // const [allMeals, setAllMeals] = useState([]);
+  // const [allDrinks, setAllDrinks] = useState([]);
   const [drinksCards, setDrinkCards] = useState([]);
   const [mealsCards, setMealsCards] = useState([]);
   const [foods, setFoods] = useState([]);
@@ -18,17 +23,24 @@ function AppRecipesProvider({ children }) {
   useEffect(() => {
     async function mealsDataForCards() {
       const cards = await fetchMealsCards();
+      // const cards12List = cards.meals.slice(ZERO, TWELVE);
       setMealsCards(cards);
+      // setAllMeals(cards.meals);
     }
 
     async function drinksDataForCards() {
       const cards = await fetchDrinksCards();
+      // const cards12List = cards.drinks.slice(ZERO, TWELVE);
       setDrinkCards(cards);
+      // setAllDrinks(cards.drinks);
     }
 
     mealsDataForCards();
     drinksDataForCards();
   }, []);
+
+  // console.log(allMeals);
+  // console.log(allDrinks);
 
   async function fetchApi(url) {
     const request = await fetch(url);
--- src/service/fetchCards.js
+++ src/service/fetchCards.js
@@ -14,3 +14,43 @@ export async function fetchDrinksCards() {
   const result = await response.json();
   return result.drinks.slice(ZERO, TWELVE);
 }
+
+// export async function fetchCategoryMeals(category) {
+//   let url;
+//   switch (category) {
+//   case 'Beef': {
+//     url = 'https://www.themealdb.com/api/json/v1/1/filter.php?c=Beef';
+//     break;
+//   }
+//   case 'Breakfast': {
+//     url = 'https://www.themealdb.com/api/json/v1/1/filter.php?c=Breakfast';
+//     break;
+//   }
+//   case 'Chicken': {
+//     url = 'https://www.themealdb.com/api/json/v1/1/filter.php?c=Chicken';
+//     break;
+//   }
+//   case 'Dessert': {
+//     url = 'https://www.themealdb.com/api/json/v1/1/filter.php?c=Dessert';
+//     break;
+//   }
+//   case 'Goat': {
+//     url = 'https://www.themealdb.com/api/json/v1/1/filter.php?c=Goat';
+//   }
+//   default:
+//     return null;
+//   }
+// }
+export async function fetchCategoryMeals(category) {
+  const url = `https://www.themealdb.com/api/json/v1/1/filter.php?c=${category}`;
+  const response = await fetch(url);
+  const result = await response.json();
+  return result.meals;
+}
+
+export async function fetchCategoryDrinks(category) {
+  const url = `https://www.thecocktaildb.com/api/json/v1/1/filter.php?c=${category}`;
+  const response = await fetch(url);
+  const result = await response.json();
+  return result.drinks;
+}
