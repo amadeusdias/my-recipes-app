0debe998c9108afb7f57683bdd96922a4725b2bf
rafaelftourinho <rafaelftourinho@gmail.com>
2022-09-27 00:55:47 -0300

    :sparkles: Requisite 20

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -1,14 +1,39 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import AppRecipesContext from '../context/AppRecipesContext';
 
+const ZERO = 0;
+const FIVE = 5;
+
 function Cards({ path }) {
-  const { drinksCards, mealsCards } = useContext(AppRecipesContext);
+  const { drinksCards,
+    mealsCards,
+  } = useContext(AppRecipesContext);
+  const [list5Meals, setList5Meals] = useState([]);
+  const [list5Drinks, setList5Drinks] = useState([]);
 
-  console.log(mealsCards);
-  console.log(path);
+  useEffect(() => {
+    async function get5MealsCategory() {
+      const url = 'https://www.themealdb.com/api/json/v1/1/list.php?c=list';
+      const response = await fetch(url);
+      const result = await response.json();
+      setList5Meals(result.meals.slice(ZERO, FIVE));
+    }
+
+    async function get5DrinksCategory() {
+      const url = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?c=list';
+      const response = await fetch(url);
+      const result = await response.json();
+      setList5Drinks(result.drinks.slice(ZERO, FIVE));
+    }
+    get5MealsCategory();
+    get5DrinksCategory();
+  }, []);
+
+  console.log(list5Meals);
+  // console.log(list5Drinks);
+
   let render = [];
-
   if (path === '/meals') {
     render = mealsCards;
   }
@@ -16,8 +41,36 @@ function Cards({ path }) {
   if (path === '/drinks') {
     render = drinksCards;
   }
+
+  // console.log(path);
+
   return (
     <div>
+      {list5Meals && path === '/meals' && list5Meals.map((item, index) => (
+        <div
+          key={ index }
+        >
+          <button
+            type="button"
+            data-testid={ `${item.strCategory}-category-filter` }
+          >
+            {item.strCategory}
+          </button>
+        </div>
+      ))}
+      {list5Drinks && path === '/drinks' && list5Drinks.map((item, index) => (
+        <div
+          key={ index }
+          id=""
+        >
+          <button
+            type="button"
+            data-testid={ `${item.strCategory}-category-filter` }
+          >
+            {item.strCategory}
+          </button>
+        </div>
+      ))}
       {render.map((item, index) => (
         <div
           key={ index }
