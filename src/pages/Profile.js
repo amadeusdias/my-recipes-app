095c4012f8686cb767b92432110944754e246c06
rafaelftourinho <rafaelftourinho@gmail.com>
2022-09-28 01:49:14 -0300

    :sparkles: Requisite 61

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,4 +1,5 @@
 import React, { useContext } from 'react';
+import { useHistory } from 'react-router-dom';
 import Footer from '../components/Footer';
 import Header from '../components/Header';
 import AppRecipesContext from '../context/AppRecipesContext';
@@ -7,15 +8,44 @@ import useLocalStorage from '../hooks/useLocalStorage';
 function Profile() {
   const { userEmail } = useContext(AppRecipesContext);
   const [localStorage] = useLocalStorage('user', userEmail);
-  if (localStorage) console.log(localStorage);
+  const history = useHistory();
+
+  function handleClickToDoneRecipes() {
+    history.push('/done-recipes');
+  }
+
+  // function handleClickToFavoriteRecipes() {
+  //   history.push('/favorite-recipes');
+  // }
+
   return (
     <div>
       <Header />
       <Footer />
       <p data-testid="profile-email">{localStorage.email}</p>
-      <button type="button" data-testid="profile-done-btn">Done Recipes</button>
-      <button type="button" data-testid="profile-favorite-btn">Favorite Recipes</button>
-      <button type="button" data-testid="profile-logout-btn">Logout</button>
+      <button
+        type="button"
+        data-testid="profile-done-btn"
+        onClick={ handleClickToDoneRecipes }
+      >
+        Done Recipes
+      </button>
+
+      <button
+        type="button"
+        data-testid="profile-favorite-btn"
+        // onClick={ handleClickToFavoriteRecipes }
+      >
+        Favorite Recipes
+
+      </button>
+
+      <button
+        type="button"
+        data-testid="profile-logout-btn"
+      >
+        Logout
+      </button>
     </div>
   );
 }
