2651093025db1036bb1c62f7663e6f88d04c9838
rafaelftourinho <rafaelftourinho@gmail.com>
2022-09-27 21:46:09 -0300

    :white_check_mark: fix 23 and test 9

--- src/tests/Header.spec.js
+++ src/tests/Header.spec.js
@@ -1,32 +1,47 @@
 import React from 'react';
 import { screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
+import { act } from 'react-dom/test-utils';
 import renderWithRouter from './helpers/renderWithRouter';
-// import { useLocation, useHistory } from 'react-router-dom';
-import Meals from '../pages/Meals';
+import App from '../App';
 
 describe('Testa o Header', () => {
   it('Testa o Header', () => {
-    renderWithRouter(<Meals />);
+    const { history } = renderWithRouter(<App />);
+    act(() => history.push('/meals'));
     const profileButton = screen.getByTestId('profile-top-btn');
     expect(profileButton).toBeInTheDocument();
   });
   it('Testa o nome da pagina', () => {
-    renderWithRouter(<Meals />);
+    const { history } = renderWithRouter(<App />);
+    act(() => history.push('/meals'));
     const pageName = screen.getByTestId('page-title');
     expect(pageName).toBeInTheDocument();
   });
-
-  it('Verifica se ao clicar no botão "Profile", o usuário é redirecionado para a página "Profile"', () => {
-    renderWithRouter(<Meals />);
-
-    const profileButton = screen.getByTestId('profile-top-btn');
-    userEvent.click(profileButton);
-    renderWithRouter(<Meals />, { initialEntries: ['/profile'] });
+  it('Testa o Search Icon', () => {
+    const { history } = renderWithRouter(<App />);
+    act(() => history.push('/meals'));
+    const search = screen.getByTestId('search-top-btn');
+    expect(search).toBeInTheDocument();
   });
-//   it('Testa o Search Icon', () => {
-//     renderWithRouter(<Meals />);
-//     const searchIcon = screen.getByTestId('search-top-bt');
-//     expect(searchIcon).toBeInTheDocument();
-//   });
+  it('Testa a funcionalidade do searchIcon', async () => {
+    const { history } = renderWithRouter(<App />);
+    act(() => history.push('/meals'));
+    const search = await screen.findByTestId('search-top-btn');
+    userEvent.click(search);
+    expect(screen.findByTestId('search-input'));
+  });
+  it('Testa o botão Profile', async () => {
+    const { history } = renderWithRouter(<App />);
+    act(() => history.push('/meals'));
+    const profileButton = await screen.getByTestId('profile-top-btn');
+    userEvent.click(profileButton);
+    expect(history.location.pathname).toBe('/profile');
+  });
+  it('Testa o nome da pagina', () => {
+    const { history } = renderWithRouter(<App />);
+    act(() => history.push('/favorite-recipes'));
+    const pageNam = screen.getByText(/Favorite Recipes/i);
+    expect(pageNam).toHaveTextContent('Favorite Recipes');
+  });
 });
