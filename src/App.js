b2f46ff0e49c67f2f9a2c0474cb16b260562ef26
lucasfaria95 <lucasfernandofaria@gmail.com>
2022-09-28 17:29:18 -0300

    requisito 37

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import DrinksDetails from './components/DrinksDetails';
+import MealsDetails from './components/MealsDetails';
+import RecipeDrinkInProgress from './components/RecipeDrinkInProgress';
+import RecipeMealsInProgress from './components/RecipeInProgress';
+import AppRecipesProvider from './context/AppRecipesProvider';
+import DoneRecipes from './pages/DoneRecipes';
+import Drinks from './pages/Drinks';
+import FavoriteRecipes from './pages/FavoriteRecipes';
 import Login from './pages/Login';
 import Meals from './pages/Meals';
-import MealsDetails from './components/MealsDetails';
-import Drinks from './pages/Drinks';
-import DrinksDetails from './components/DrinksDetails';
-import DoneRecipes from './pages/DoneRecipes';
-import FavoriteRecipes from './pages/FavoriteRecipes';
 import Profile from './pages/Profile';
-import AppRecipesProvider from './context/AppRecipesProvider';
 
 function App() {
   return (
@@ -18,10 +20,10 @@ function App() {
         <Route exact path="/" component={ Login } />
         <Route exact path="/meals" component={ Meals } />
         <Route exact path="/drinks" component={ Drinks } />
+        <Route path="/meals/:id/in-progress" component={ RecipeMealsInProgress } />
         <Route path="/meals/:id" component={ MealsDetails } />
+        <Route path="/drinks/:id/in-progress" component={ RecipeDrinkInProgress } />
         <Route path="/drinks/:id" component={ DrinksDetails } />
-        <Route path="/drinks/:id/in-progress" />
-        <Route path="/meals/:id/in-progress" />
         <Route path="/profile" component={ Profile } />
         <Route path="/done-recipes" component={ DoneRecipes } />
         <Route path="/favorite-recipes" component={ FavoriteRecipes } />
