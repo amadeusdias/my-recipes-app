130637b87aee512afa155ffb1638a2c35fbc4f1e
rafaelftourinho <rafaelftourinho@gmail.com>
2022-09-28 01:25:41 -0300

    :sparkles: Requisite 57, 58 e 59

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,13 +1,21 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import Footer from '../components/Footer';
 import Header from '../components/Header';
+import AppRecipesContext from '../context/AppRecipesContext';
+import useLocalStorage from '../hooks/useLocalStorage';
 
 function Profile() {
+  const { userEmail } = useContext(AppRecipesContext);
+  const [localStorage] = useLocalStorage('user', userEmail);
+  if (localStorage) console.log(localStorage);
   return (
     <div>
       <Header />
       <Footer />
-      Profile
+      <p data-testid="profile-email">{localStorage.email}</p>
+      <button type="button" data-testid="profile-done-btn">Done Recipes</button>
+      <button type="button" data-testid="profile-favorite-btn">Favorite Recipes</button>
+      <button type="button" data-testid="profile-logout-btn">Logout</button>
     </div>
   );
 }
