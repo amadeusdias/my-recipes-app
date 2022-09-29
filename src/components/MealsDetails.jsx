5b9f2f3b275b9c31db4a2c04b35c026bc5b6a3c5
rafaelftourinho <rafaelftourinho@gmail.com>
2022-09-29 01:33:58 -0300

    :sparkles: Requisite 28

--- src/components/MealsDetails.jsx
+++ src/components/MealsDetails.jsx
@@ -4,12 +4,14 @@ import { useParams } from 'react-router-dom';
 import YoutubeEmbed from './YoutubeEmbed';
 import AppRecipesContext from '../context/AppRecipesContext';
 import numbers from '../tests/helpers/numbers';
+import '../css/carousel.css';
 
+const SIX = 6;
 function MealsDetails({ match: { params: { id } } }) {
   const { mealsCards } = useContext(AppRecipesContext);
   const [findMeal, setFindMeal] = useState([]);
   const [returnApiMeals, setReturnApiMeals] = useState([]);
-  // const [returnAllMeals, setReturnAllMeals] = useState([]);
+  const [returnAllDrinks, setReturnAllDrinks] = useState([]);
   const params = useParams();
 
   const cleanEmpty = (obj) => {
@@ -33,6 +35,17 @@ function MealsDetails({ match: { params: { id } } }) {
 
     fetchMealsDetails();
   }, []); // eslint-disable-line
+
+  useEffect(() => {
+    const fetchSixDrinksRecommended = async () => {
+      const url = 'https://www.thecocktaildb.com/api/json/v1/1/search.php?s=';
+      const response = await fetch(url);
+      const result = await response.json();
+      setReturnAllDrinks(result.drinks);
+    };
+
+    fetchSixDrinksRecommended();
+  }, []);
 
   const TRINTAEDOIS = 32;
 
@@ -86,6 +99,36 @@ function MealsDetails({ match: { params: { id } } }) {
           </div>
         </div>
       ))}
+      <div className="scroll">
+        {returnAllDrinks.length > 0 && returnAllDrinks.map((drink, index) => (
+          index < SIX && (
+            <div
+              className="scroll-child"
+              key={ drink.idDrink }
+              data-testid={ `${index}-recommendation-card` }
+            >
+              <p
+                className="p-scroll"
+                data-testid={ `${index}-recommendation-title` }
+              >
+                {drink.strDrink}
+              </p>
+              <img
+                className="img-scroll"
+                src={ drink.strDrinkThumb }
+                alt={ drink.strDrink }
+              />
+            </div>
+          )
+        ))}
+      </div>
+      <button
+        className="scroll-btn"
+        type="button"
+        data-testid="start-recipe-btn"
+      >
+        Start Recipe
+      </button>
     </div>
   );
 }
