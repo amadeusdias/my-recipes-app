a3e1193bdee141baf3e1d415ca92fdb745ea01d2
lucasfaria95 <lucasfernandofaria@gmail.com>
2022-09-29 12:08:34 -0300

    requisito 38 e 39 incompleto

--- src/components/RecipeInProgress.jsx
+++ src/components/RecipeInProgress.jsx
@@ -1,21 +1,51 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 
+const VINTE = 20;
+
 function RecipeInProgress() {
+  const [returnApiMeals, setReturnApiMeals] = useState([]);
   const params = useParams();
-  console.log(params);
+  const ingredients = [];
+
+  useEffect(() => {
+    const fetchMealsDetails = async () => {
+      const url = `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${params.id}`;
+      const response = await fetch(url);
+      const result = await response.json();
+      setReturnApiMeals(result.meals[0]);
+    };
+
+    fetchMealsDetails();
+  }, []);
+  for (let index = 0; index <= VINTE; index += 1) {
+    if (returnApiMeals && returnApiMeals[`strIngredient${index}`]) {
+      ingredients.push(returnApiMeals[`strIngredient${index}`]);
+    }
+  }
+
+  console.log(ingredients);
+
   return (
     <div>
-      <p data-testid="recipe-title">titulo da receita</p>
+      <p data-testid="recipe-title">{returnApiMeals.strArea}</p>
       <img
         data-testid="recipe-photo"
-        src="Link da imagem"
-        alt="Link da imagem"
+        src={ returnApiMeals.strMealThumb }
+        alt={ returnApiMeals.strArea }
       />
       <button type="button" data-testid="share-btn">Compartilhar</button>
       <button type="button" data-testid="favorite-btn">Favoritar</button>
-      <p data-testid="recipe-category">texto da categoria </p>
-      <p data-testid="instructions">texto de instruções</p>
+      <p data-testid="recipe-category">{ returnApiMeals.strCategory }</p>
+      <p data-testid="instructions">{returnApiMeals.strInstructions}</p>
+
+      {ingredients.map((element) => (
+        <label key={ element } htmlFor={ element } data-testid="ingredient-step">
+          {element}
+          <input name={ element } type="checkbox" />
+        </label>
+      ))}
+
       <button type="button" data-testid="finish-recipe-btn">Finalizar</button>
     </div>
   );
