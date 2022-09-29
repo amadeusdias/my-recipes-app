c22a5a8ac294bf6f262afa1e231b3992f4864094
AmadeusDias <amadeusdias@hotmail.com>
2022-09-28 21:07:53 -0300

    req 25

--- src/components/MealsDetails.jsx
+++ src/components/MealsDetails.jsx
@@ -1,13 +1,34 @@
 import React, { useContext, useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
 import { useParams } from 'react-router-dom';
+import YoutubeEmbed from './YoutubeEmbed';
 import AppRecipesContext from '../context/AppRecipesContext';
+import numbers from '../tests/helpers/numbers';
 
 function MealsDetails({ match: { params: { id } } }) {
   const { mealsCards } = useContext(AppRecipesContext);
   const [findMeal, setFindMeal] = useState([]);
-  const [returnApiMeals, setReturnApiMeals] = useState('');
+  // const [findCleanMeal, setFindCLeanMeal] = useState([]);
+  const [returnApiMeals, setReturnApiMeals] = useState([]);
   const params = useParams();
+
+  const cleanEmpty = (obj) => {
+    const clean = Object.fromEntries(Object.entries(obj)
+      .filter(([, v]) => v != null)
+      .filter(([, v]) => v !== '')
+      .filter(([, v]) => v !== ' '));
+    return Array(clean);
+  };
+  console.log(numbers);
+  console.log(findMeal);
+
+  const cleanObj = (obj) => {
+    const clean = Object.fromEntries(Object.entries(obj)
+      .filter(([, v]) => v != null)
+      .filter(([, v]) => v !== '')
+      .filter(([, v]) => v !== ' '));
+    return (clean);
+  };
 
   useEffect(() => {
     setFindMeal(mealsCards.filter((meal) => meal.idMeal === id));
@@ -22,16 +43,45 @@ function MealsDetails({ match: { params: { id } } }) {
     };
 
     fetchMealsDetails();
-  });
+  }, []);
 
-  console.log(returnApiMeals);
+  // const entries = cleanEmpty(returnApiMeals);
+  // console.log((Object.entries(entries)).flat());
+  console.log(cleanObj(returnApiMeals) ? cleanEmpty(returnApiMeals)
+    .find((a, i) => Object.keys(a)) : null);
+  // console.log(Object.entries(cleanEmpty(returnApiMeals)[0]).filter((a) => a));
+
+  const TRINTAEDOIS = 32;
 
   return (
     <div>
-      {findMeal.map((item, index) => (
+      {cleanEmpty(returnApiMeals).map((item, index) => (
         <div key={ index }>
-          <p>{item.strInstructions}</p>
-          <img src={ item.strMealThumb } alt={ item.strMeal } />
+          <h3 data-testid="recipe-title">{item.strMeal}</h3>
+          <p data-testid="recipe-category">
+            Category:
+            {item.strCategory}
+          </p>
+          <ul>
+            qualquer
+          </ul>
+          <p data-testid="instructions">
+            Instructions:
+            {item.strInstructions}
+          </p>
+          <img
+            data-testid="recipe-photo"
+            src={ item.strMealThumb }
+            alt={ item.strMeal }
+          />
+          <div>
+            <YoutubeEmbed
+              data-testid="video"
+              embedId={ item.strYoutube
+                ? item.strYoutube.slice(TRINTAEDOIS) : null }
+            />
+
+          </div>
         </div>
       ))}
     </div>
